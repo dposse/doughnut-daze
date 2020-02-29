4c2dce131bf4eb768aa2a9ba4a6cf2f198b675e0
dposse <dposse.125@gmail.com>
2020-02-29 15:05:43 -0500

    Remove font awesome arrow

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,8 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { FaArrowDown } from "react-icons/fa/";
-
 const Hero = props => {
   const { scrollToContent, backgrounds, theme } = props;
 
